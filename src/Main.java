c01ade827852f93fa237c6b37431c59e693f6812
Igor Mitin <igor.mitin19969@gmail.com>
2025-05-30 11:56:18 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main (String[] args) {
+        int ticket = 18645;
+        int bonus = ticket / 20;
+        System.out.println("Количество начисленных миль: " + bonus);
+    }
+}
