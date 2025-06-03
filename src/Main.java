95e9fdf46d8561d4031adf7db79852fda4b3aed0
Igor Mitin <igor.mitin19969@gmail.com>
2025-06-03 14:24:58 +0700

    cheange Main,BonusMilesService

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
-    public static void main (String[] args) {
-        int ticket = 18645;
-        int bonus = ticket / 20;
-        System.out.println("Количество начисленных миль: " + bonus);
-    }
+    public static void main(String[] args) {
+
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println("Количество начисленных миль: " + miles);
+  }
 }
